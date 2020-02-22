bdded38510f0123f4f9805b674c789c80812f4dc
damoklov <mishanya@protonmail.com>
2020-02-22 23:14:21 +0200

    Implemented required sorting methods

--- src/main/java/ua/lviv/iot/appliance/manager/HomeApplianceManagerUtils.java
+++ src/main/java/ua/lviv/iot/appliance/manager/HomeApplianceManagerUtils.java
@@ -3,49 +3,49 @@ package ua.lviv.iot.appliance.manager;
 import ua.lviv.iot.appliance.model.HomeAppliance;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class HomeApplianceManagerUtils {
-    public static void sortHomeApplianceByPowerUsage(ArrayList<HomeAppliance> homeApplianceList, SortType sortType){
-        Collections.sort(homeApplianceList, powerComparator);
-        if (sortType == SortType.ASCENDING){
-            Collections.reverse(homeApplianceList);
+
+    private static final HomeApplianceSorterByName APPLIANCE_NAME_SORTER = new HomeApplianceSorterByName();
+
+    static class HomeApplianceSorterByName implements Comparator<HomeAppliance> {
+        @Override
+        public int compare(HomeAppliance firstAppliance, HomeAppliance secondAppliance) {
+            return firstAppliance.getApplianceName().compareTo(secondAppliance.getApplianceName());
         }
+    }
+
+    class HomeApplianceSorterByPowerUsage implements Comparator<HomeAppliance> {
+        @Override
+        public int compare(HomeAppliance firstAppliance, HomeAppliance secondAppliance) {
+            return firstAppliance.getPowerConsumption() - secondAppliance.getPowerConsumption();
+        }
+    }
+
+    public static void sortHomeApplianceByTimeUsage(ArrayList<HomeAppliance> homeApplianceList, SortType sortType) {
+        /* Anonymous Inner Class */
+
+        Comparator<HomeAppliance> comparator = new Comparator<HomeAppliance>() {
+
+            @Override
+            public int compare(HomeAppliance firstAppliance, HomeAppliance secondAppliance) {
+                return (Double.compare(firstAppliance.getHoursPerMonthUsage(), secondAppliance.getHoursPerMonthUsage()));
+            }
+        };
+        homeApplianceList.sort(sortType == SortType.ASCENDING ? comparator : comparator.reversed());
+    }
+
+    public static void sortHomeApplianceByPowerUsage(ArrayList<HomeAppliance> homeApplianceList, SortType sortType){
+        Comparator<HomeAppliance> comparator = Comparator.comparing(HomeAppliance::getPowerConsumption);
+        homeApplianceList.sort(sortType == SortType.ASCENDING ? comparator : comparator.reversed());
     }
 
     public static void sortHomeApplianceByName(ArrayList<HomeAppliance> homeApplianceList, SortType sortType){
-        Collections.sort(homeApplianceList, nameComparator);
-        if (sortType == SortType.DESCENDING){
-            Collections.reverse(homeApplianceList);
-        }
+        homeApplianceList.sort(sortType == SortType.ASCENDING ? APPLIANCE_NAME_SORTER : APPLIANCE_NAME_SORTER.reversed());
     }
 
-    public static void sortHomeApplianceByTimeUsage(ArrayList<HomeAppliance> homeApplianceList, SortType sortType){
-        Collections.sort(homeApplianceList, usageComparator);
-        if (sortType == SortType.ASCENDING){
-            Collections.reverse(homeApplianceList);
-        }
+    public static void sortHomeApplianceByRepairPrice(ArrayList<HomeAppliance> homeApplianceList, SortType sortType) {
+        homeApplianceList.sort(sortType == SortType.ASCENDING ? (o1, o2) -> (int) (o1.getRepairPrice() - o2.getRepairPrice()) : (o1, o2) -> (int) (o2.getRepairPrice() - o1.getRepairPrice()));
     }
-
-    private static Comparator<HomeAppliance> powerComparator = new Comparator<>() {
-        @Override
-        public int compare(HomeAppliance homeAppliance1, HomeAppliance homeAppliance2) {
-            return (Integer.compare(homeAppliance2.getPowerConsumption(), homeAppliance1.getPowerConsumption()));
-        }
-    };
-
-    private static Comparator<HomeAppliance> nameComparator = new Comparator<>() {
-        @Override
-        public int compare(HomeAppliance homeAppliance1, HomeAppliance homeAppliance2) {
-            return (homeAppliance1.getApplianceName().compareTo(homeAppliance2.getApplianceName()));
-        }
-    };
-
-    private static Comparator<HomeAppliance> usageComparator = new Comparator<>() {
-        @Override
-        public int compare(HomeAppliance homeAppliance1, HomeAppliance homeAppliance2) {
-            return (Double.compare(homeAppliance2.getHoursPerMonthUsage(), homeAppliance1.getHoursPerMonthUsage()));
-        }
-    };
 }
