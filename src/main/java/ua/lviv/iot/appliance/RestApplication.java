5a3e3c76318edf12c586130f509cecc2d652298a
damoklov <mishanya@protonmail.com>
2020-04-04 21:41:01 +0300

    Updated REST application executable

--- src/main/java/ua/lviv/iot/appliance/RestApplication.java
+++ src/main/java/ua/lviv/iot/appliance/RestApplication.java
@@ -2,8 +2,15 @@ package ua.lviv.iot.appliance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("ua.lviv.iot.appliance.dataaccess")
+@ComponentScan({
+        "ua.lviv.iot.appliance.controller",
+        "ua.lviv.iot.appliance.dataaccess",
+        "ua.lviv.iot.appliance.business" })
 public class RestApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestApplication.class, args);
