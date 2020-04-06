956aabc31fe30f528c823c0225d3953582972f67
damoklov <mishanya@protonmail.com>
2020-04-06 14:07:49 +0300

    Updated Controller file

--- src/main/java/ua/lviv/iot/appliance/controller/HomeApplianceController.java
+++ src/main/java/ua/lviv/iot/appliance/controller/HomeApplianceController.java
@@ -1,10 +1,6 @@
 package ua.lviv.iot.appliance.controller;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,39 +16,33 @@ import ua.lviv.iot.appliance.model.HomeAppliance;
 @RestController
 public class HomeApplianceController {
 
-  private Map<Integer, HomeAppliance> homeAppliance = new HashMap<>();
-  private AtomicInteger idCounter = new AtomicInteger();
-
   @Autowired
   private HomeApplianceService homeApplianceService;
 
   @GetMapping(path = "/{id}")
-  public HomeAppliance getHomeAppliance(@PathVariable("id") Integer applianceId) {
-    return homeAppliance.get(applianceId);
+  public ResponseEntity<HomeAppliance> getHomeAppliance(@PathVariable("id") Integer applianceId) {
+    return homeApplianceService.getHomeAppliance(applianceId);
   }
 
   @GetMapping
   public List<HomeAppliance> getAllHomeAppliance() {
-    return new LinkedList<>(homeAppliance.values());
+    return homeApplianceService.getAllHomeAppliance();
   }
 
   @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   public HomeAppliance createHomeAppliance(@RequestBody HomeAppliance appliance) {
-    appliance.setId(idCounter.incrementAndGet());
-    homeAppliance.put(appliance.getId(), appliance);
     homeApplianceService.createHomeAppliance(appliance);
     return appliance;
   }
 
   @PutMapping(path = "/{id}")
-  public HomeAppliance updateHomeAppliance(@PathVariable("id") Integer applianceId, @RequestBody HomeAppliance appliance) {
-    appliance.setId(applianceId);
-    return homeAppliance.put(applianceId, appliance);
+  public ResponseEntity<HomeAppliance>  updateHomeAppliance(@PathVariable("id") Integer applianceId, @RequestBody HomeAppliance appliance) {
+    return homeApplianceService.updateHomeAppliance(appliance, applianceId);
   }
 
   @DeleteMapping(path = "/{id}")
-  public ResponseEntity deleteHomeAppliance(@PathVariable("id") Integer applianceId) {
-    HttpStatus status = homeAppliance.remove(applianceId) == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+  public ResponseEntity<HomeAppliance> deleteHomeAppliance(@PathVariable("id") Integer applianceId) {
+    HttpStatus status = homeApplianceService.deleteHomeAppliance(applianceId);
     return ResponseEntity.status(status).build();
   }
 
