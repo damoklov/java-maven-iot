117dcae828c8ff23040870a00a96443758f3b02c
damoklov <mishanya@protonmail.com>
2020-04-11 00:34:37 +0300

    Added controllers for new classes

--- src/main/java/ua/lviv/iot/appliance/controller/ManufacturerController.java
+++ src/main/java/ua/lviv/iot/appliance/controller/ManufacturerController.java
@@ -1,4 +1,53 @@
 package ua.lviv.iot.appliance.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ua.lviv.iot.appliance.business.ManufacturerService;
+import ua.lviv.iot.appliance.model.Manufacturer;
+
+@RequestMapping("/manufacturers")
+@RestController
 public class ManufacturerController {
+
+  @Autowired
+  private ManufacturerService manufacturerService;
+
+  @GetMapping
+  public List<Manufacturer> getAllManufacturers() {
+    return manufacturerService.findAll();
+  }
+
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<Manufacturer> getManufacturer(@PathVariable("id") Integer manufacturerId) {
+    return manufacturerService.getManufacturer(manufacturerId);
+  }
+
+  @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  public Manufacturer createManufacturer(@RequestBody Manufacturer manufacturer) {
+    manufacturerService.createManufacturer(manufacturer);
+    return manufacturer;
+  }
+
+  @PutMapping(path = "/{id}")
+  public ResponseEntity<Manufacturer>  updateManufacturer(@PathVariable("id") Integer manufacturerId, @RequestBody Manufacturer manufacturer) {
+    return manufacturerService.updateManufacturer(manufacturer, manufacturerId);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Manufacturer> deleteManufacturer(@PathVariable("id") Integer manufacturerId) {
+    HttpStatus status = manufacturerService.deleteManufacturer(manufacturerId);
+    return ResponseEntity.status(status).build();
+  }
+
+  public ManufacturerService getManufacturerService() {
+    return manufacturerService;
+  }
+
+  public void setManufacturerService(ManufacturerService manufacturerService) {
+    this.manufacturerService = manufacturerService;
+  }
 }
--- src/main/java/ua/lviv/iot/appliance/controller/OwnerController.java
+++ src/main/java/ua/lviv/iot/appliance/controller/OwnerController.java
@@ -1,4 +1,43 @@
 package ua.lviv.iot.appliance.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ua.lviv.iot.appliance.business.OwnerService;
+import ua.lviv.iot.appliance.model.Owner;
+
+@RequestMapping("/owners")
+@RestController
 public class OwnerController {
+  @Autowired
+  private OwnerService ownerService;
+
+  @GetMapping
+  public List<Owner> getAllOwners() {
+    return this.ownerService.findAll();
+  }
+
+  @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  public Owner createOwner(@RequestBody Owner owner) {
+    ownerService.createOwner(owner);
+    return owner;
+  }
+  @GetMapping(path = "/{id}")
+  public ResponseEntity<Owner> getOwner(@PathVariable("id") Integer ownerId) {
+    return ownerService.getOwner(ownerId);
+  }
+
+  @PutMapping(path = "/{id}")
+  public ResponseEntity<Owner>  updateOwner(@PathVariable("id") Integer ownerId, @RequestBody Owner owner) {
+    return ownerService.updateOwner(owner, ownerId);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Owner> deleteOwner(@PathVariable("id") Integer ownerId) {
+    HttpStatus status = ownerService.deleteOwner(ownerId);
+    return ResponseEntity.status(status).build();
+  }
 }
