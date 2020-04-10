b0ed41c58aa87fc58fd096391ae31516b1548ea3
damoklov <mishanya@protonmail.com>
2020-04-11 00:35:10 +0300

    Added new classes to data-access level

--- src/main/java/ua/lviv/iot/appliance/dataaccess/ManufacturerRepository.java
+++ src/main/java/ua/lviv/iot/appliance/dataaccess/ManufacturerRepository.java
@@ -1,4 +1,10 @@
 package ua.lviv.iot.appliance.dataaccess;
 
-public interface ManufacturerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.lviv.iot.appliance.model.Manufacturer;
+
+@Repository
+public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer> {
+
 }
--- src/main/java/ua/lviv/iot/appliance/dataaccess/OwnerRepository.java
+++ src/main/java/ua/lviv/iot/appliance/dataaccess/OwnerRepository.java
@@ -1,4 +1,10 @@
 package ua.lviv.iot.appliance.dataaccess;
 
-public interface OwnerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.lviv.iot.appliance.model.Owner;
+
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+
 }
